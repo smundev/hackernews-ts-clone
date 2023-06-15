ff78905701f2fa2407978b0060d6e3446ec775d2
Samuel Mundo <samuelmundo@gmail.com>
2023-06-15 17:34:42 -0600

    feat/adding-prev-next-pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { gql, useQuery } from "@apollo/client";
 import Story from "./Story";
 import { RingLoader } from "react-spinners";
+import { useState } from "react";
 
-const GetTopStories = gql`
-  query {
-    GetTopStories {
+const GET_TOP_STORIES = gql`
+  query GetTopStories($offset: Int, $limit: Int) {
+    GetTopStories(offset: $offset, limit: $limit) {
       id
       by
       kids
@@ -18,32 +19,73 @@ const GetTopStories = gql`
   }
 `;
 
+const PAGINATION_LIMIT = 100;
+enum PAGINATION_DIRECTION {
+  NEXT = "NEXT",
+  PREVIOUS = "PREVIOUS",
+}
+
 function App() {
-  const { data, loading } = useQuery(GetTopStories);
+  const [offset, setOffset] = useState(0);
+  const { data, loading } = useQuery(GET_TOP_STORIES, {
+    variables: { offset, limit: PAGINATION_LIMIT },
+  });
+
+  const handleOffset = (direction: PAGINATION_DIRECTION) => {
+    setOffset((prev) => {
+      if (direction === PAGINATION_DIRECTION.PREVIOUS) return prev - 1;
+      if (direction === PAGINATION_DIRECTION.NEXT) return prev + 1;
+      return prev;
+    });
+  };
 
   return (
     <>
       <main className="container">
         <header className="header">
           <a href="/">
-            <img src="public/y18.svg" alt="hacker news logo" />
+            <img src="/y18.svg" alt="hacker news logo" />
           </a>
-
           <a href="/">
             <strong>Hacker News</strong>
           </a>
         </header>
+        <section className="story-container">
+          {data?.GetTopStories.map((item: Story, i: number) => (
+            <Story
+              key={item.id}
+              story={item}
+              index={offset * PAGINATION_LIMIT + i}
+            />
+          ))}
+        </section>
         {loading && (
           <div className="loader">
             {<RingLoader size={50} color="#ff6600" />}
           </div>
         )}
-
-        <section className="story-container">
-          {data?.GetTopStories.map((item: Story, i: number) => (
-            <Story key={item.id} story={item} index={i} />
-          ))}
-        </section>
+        {!loading && (
+          <div className="page-button-group">
+            {offset !== 0 && (
+              <button
+                role="button"
+                className="load-more"
+                onClick={() => handleOffset(PAGINATION_DIRECTION.PREVIOUS)}
+              >
+                Previous
+              </button>
+            )}
+            {data?.GetTopStories.length !== 0 && (
+              <button
+                role="button"
+                className="load-more"
+                onClick={() => handleOffset(PAGINATION_DIRECTION.NEXT)}
+              >
+                Next
+              </button>
+            )}
+          </div>
+        )}
       </main>
     </>
   );
